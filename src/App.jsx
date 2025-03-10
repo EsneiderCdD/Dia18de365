ca3ecb893c50c5d9c449cc0bb66ca6428d4b4024
ecd.97@hotmail.com <ecd.97@hotmail.com>
2025-03-09 23:02:06 -0500

    Nuevo Slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Class01_BasicSlider from './components/Class01_BasicSlider'
 import Class02_NavSlider from './components/Class02_NavSlider'
 import Class03_AutoPlaySlider from './components/Class03_AutoPlaySlider'
 import MultiItemSlider from './components/MultiItemSlider/MultiItemSlider'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -13,10 +14,17 @@ function App() {
       
         <div>
        
-          <Class01_BasicSlider />
-          <Class02_NavSlider />
-          <Class03_AutoPlaySlider />
-          <MultiItemSlider />
+          
+            <Class01_BasicSlider />
+            <Class02_NavSlider />
+            <Class03_AutoPlaySlider />
+            <MultiItemSlider></MultiItemSlider>
+          
+            
+          
+          
+
+          
 
         </div>
     
